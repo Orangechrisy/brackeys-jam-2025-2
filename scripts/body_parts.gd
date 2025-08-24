c7ccf017afd562fdc2ef0aad9b57bd5f9805925a
Orangechrisy <chris@till.com>
2025-08-24 15:44:21 -0700

    some more scene starts

---
+++ scripts/body_parts.gd
@@ -0,0 +1,4 @@
+class_name BodyParts extends Resource
+
+@export var name : StringName
+@export var image : Texture2D
---
+++ scripts/game_manager.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@export var body_parts = []
+
+func _ready() -> void:
+	pass
